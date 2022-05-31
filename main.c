27baa5b5e5f5c6d8e081c0ec953fed2f9ccd8b69
MatiasHermidaUTN <83124794+MatiasHermidaUTN@users.noreply.github.com>
2022-05-30 22:34:22 -0300

    Hago correcciones mencionadas en el mail

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 * Autor: Matias Ezequiel Hermida
 * Materia: Sintaxis y Semantica de los Lenguajes
 *
-* No considero letras a la'ñ' ni de vocales con con acento ni dieresis
+* No considero letras a la'Ã±' ni de vocales con con acento ni dieresis
 */
 
 #include <stdio.h>
@@ -11,20 +11,17 @@
 int main()
 {
     int x;
-    while(1)
+    printf("\n Ingrese caracter o frase: ");
+    x = getchar();
+    while(x != EOF)
     {
-        printf("\n Ingrese caracter o frase (lee hasta \\n, no incluido): ");
-        x = getchar();
-        while(x != '\n')
-        {
-            if (isupper(x))
-                printf("La letra es: %c, y su minuscula %c \n",x,tolower(x));
-            else if (islower(x))
-                printf("La letra es: %c, y su mayuscula %c \n",x,toupper(x));
-            else if (!isdigit(x))
-                printf("El caracter ingresado fue: %c \n",x);
-            x = getchar ();
-        }
+        if (isupper(x))
+            printf("%c",tolower(x));
+        else if (islower(x))
+            printf("%c",toupper(x));
+        else if (!isdigit(x))
+            printf("%c",x);
+        x = getchar ();
     }
     return 0;
 }
